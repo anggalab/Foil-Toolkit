4a83a312b72e5cc0e4f5570ca2f539a9241d435d
anggalab <60284991+anggalab@users.noreply.github.com>
2020-04-08 21:26:43 +0700

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,8 +1,10 @@
 import sys
 import os
 
+from subprocess import Popen
+
 from PySide2 import QtCore, QtGui
-from PySide2.QtWidgets import QApplication, QMessageBox, QFileDialog, QMainWindow, QDialog, QLineEdit, QPushButton, QStatusBar, QTextEdit, QWidget, QAction, QKeySequenceEdit, QMenuBar, QMenu, QSizePolicy, QDesktopWidget, QVBoxLayout, QPushButton, QLabel
+from PySide2.QtWidgets import QApplication, QMessageBox, QFileDialog, QMainWindow, QDialog, QLineEdit, QPushButton, QStatusBar, QWidget, QAction, QKeySequenceEdit, QMenuBar, QMenu, QSizePolicy, QDesktopWidget, QVBoxLayout, QPushButton, QLabel, QTextEdit
 from PySide2.QtGui import QIcon, QKeySequence
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -11,8 +13,14 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import numpy as np
 
-from airfoilplot import airfoil_plot
-from airfoilplot import naca
+from naca import naca
+
+from export import dat
+from export import csv
+from export import txt
+from export import xls
+from export import dxfe
+from export import flt
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -21,131 +29,168 @@ class MainWindow(QMainWindow):
         self.width = 500
         self.height = 500
         self.setFixedSize(self.size())
-        self.setWindowIcon(QIcon('img/'))
-        self.Skin()
-        self.komponen()
-#        self.center()
+        self.setWindowIcon(QIcon('img/icon.jpg'))
+        self.plotcanvas()
+        self.component()
+        self.centerwindow()
     
     def inputwindowform(self):
         self.input_window = InputWindow()
         self.input_window.show()
 
-#    def center(self):
-#        qRect = self.frameGeometry()
-#        centerPoint = QDesktopWidget().availableGeometry().center()
-#        qRect.moveCenter(centerPoint)
-#        self.move(qRect.topLeft())
+    def propertiesform(self):
+        self.properties_window = PropertiesWindow()
+        self.properties_window.show()
 
-    def Skin(self):
+    def plotcanvas(self):
         self.setWindowTitle(self.title)
         self.setGeometry(0, 0, self.width, self.height)
-        m = PlotCanvas(self, width=6.4, height=5)
+        m = PlotCanvas(self)
         m.move(0, 0)
         self.show()
-    
+
+    def centerwindow(self):
+        ctr = self.frameGeometry()
+        dsk = QDesktopWidget().availableGeometry().center()
+        ctr.moveCenter(dsk)
+        self.move(ctr.topLeft())
+
     def CreateMenus(self):
         self.fileMenu = self.menuBar().addMenu("&File")
-        self.editMenu = self.menuBar().addMenu("&Edit")
-        self.optionMenu = self.menuBar().addMenu("&Option")
+        self.viewMenu = self.menuBar().addMenu("&View")
+        self.optionMenu = self.menuBar().addMenu("&Foil")
         self.helpMenu = self.menuBar().addMenu("&Help")
 
-    def komponen(self):
+    def component(self):
         self.CreateActions()
         self.CreateMenus()
         self.CreateToolBar()
 
-        self.fileMenu.addAction(self.newAction)
-        self.fileMenu.addAction(self.DxfAction)
-        self.fileMenu.addAction(self.DatAction)
-        self.fileMenu.addAction(self.TxtAction)
+        self.fileMenu.addAction(self.openAction)
+        self.fileMenu.addAction(self.saveAction)
+
+        self.export = self.fileMenu.addMenu(QIcon('img/other.png'), "Export")
+        self.export.addAction(self.DatAction)
+        self.export.addAction(self.DxfAction)
+        self.export.addAction(self.DatAction)
+        self.export.addAction(self.TxtAction)
+        self.export.addAction(self.CsvAction)
+        self.export.addAction(self.XlsAction)
+
+        self.imp = self.fileMenu.addMenu(QIcon('img/other.png'), "Import")
+        self.imp.addAction(self.TxtImport)
+        self.imp.addAction(self.DatImport)
 
         self.fileMenu.addSeparator()
         self.fileMenu.addAction(self.exitAction)
         self.optionMenu.addAction(self.inputAction)
-        self.editMenu.addAction(self.undoAction)
-        self.editMenu.addAction(self.redoAction)
+        self.optionMenu.addAction(self.propertiesAction)
+        self.viewMenu.addAction(self.logAction)
         
         self.fileMenu.addSeparator()
         self.helpMenu.addAction(self.aboutAction)
 
-        self.mainToolBar.addAction(self.newAction)
         self.mainToolBar.addAction(self.DxfAction)
         self.mainToolBar.addAction(self.TxtAction)
         self.mainToolBar.addAction(self.DatAction)
         
         self.mainToolBar.addSeparator()
-        self.mainToolBar.addAction(self.undoAction)
-        self.mainToolBar.addAction(self.redoAction)
+        self.mainToolBar.addAction(self.logAction)
 
     def CreateActions(self):
-        self.newAction = QAction( QIcon('img/new.png'), '&New Project', self, shortcut=QKeySequence.New, statusTip="Create a New File", triggered=self.newFile)
+        self.openAction = QAction( QIcon('img/open.png'), '&Open', self, shortcut=QKeySequence.New, statusTip="Create a New File", triggered=self.openFile)
+        self.saveAction = QAction( QIcon('img/save.png'), '&Save', self, shortcut=QKeySequence.New, statusTip="Create a New File", triggered=self.saveFile)
         self.exitAction = QAction( QIcon('img/exit.png'), 'E&xit', self, shortcut="Ctrl+Q", statusTip="Exit the Application", triggered=self.exitFile)
         self.aboutAction = QAction( QIcon('img/about.png'), 'A&bout', self, statusTip="Displays info about text editor", triggered=self.showAbout)
-        self.DatAction = QAction( QIcon('img/save.png'), '&Save as DAT', self, statusTip="Save the current file to disk")
-        self.TxtAction = QAction( QIcon('img/save.png'), '&Save as TXT', self, statusTip="Save the current file to disk")
-        self.DxfAction = QAction( QIcon('img/save.png'), '&Save as DXF', self, statusTip="Save the current file to disk")
+        self.DatAction = QAction( QIcon('img/dat.png'), 'dat', self, statusTip="Save the current file to disk", triggered=self.DatExport)
+        self.TxtAction = QAction( QIcon('img/txt.png'), 'txt', self, statusTip="Save the current file to disk", triggered=self.TxtExport)
+        self.DxfAction = QAction( QIcon('img/dxf.png'), 'dxf', self, statusTip="Save the current file to disk", triggered=self.DxfExport)
+        self.CsvAction = QAction( QIcon('img/csv.png'), 'csv', self, statusTip="Save the current file to disk", triggered=self.CsvExport)
+        self.XlsAction = QAction( QIcon('img/xls.png'), 'xls', self, statusTip="Save the current file to disk", triggered=self.XlsExport)
         self.cutAction = QAction( QIcon('img/cut.png'), 'C&ut', self, statusTip="Cut the current selection to clipboard")
-        self.redoAction = QAction( QIcon('img/redo.png'),'Redo', self, statusTip="Redo previous action")
-        self.undoAction = QAction( QIcon('img/undo.png'),'Undo', self,  statusTip="Undo previous action")
-        self.inputAction = QAction( QIcon('img/redo.png'),'Foil', self, statusTip="Redo previous action", triggered=self.inputwindowform)
+        self.logAction = QAction( QIcon('img/log.png'),'Log', self, statusTip="Redo previous action", triggered=self.logView)
+        self.inputAction = QAction( QIcon('img/input.png'),'Input', self, statusTip="Redo previous action", triggered=self.inputwindowform)
+        self.propertiesAction = QAction( QIcon('img/properties.png'),'Properties', self, statusTip="Redo previous action", triggered=self.propertiesform)
+        self.DatImport = QAction( QIcon('img/dat.png'),'dat', self, statusTip="Redo previous action", triggered=self.DatImport)
+        self.TxtImport = QAction( QIcon('img/txt.png'),'txt', self, statusTip="Redo previous action", triggered=self.TxtImport)
+    
+    def DatImport(self):
+        self.fileName, self.filterName = QFileDialog.getOpenFileName(self)
+        self.textEdit.setText(open(self.fileName).read())
+
+    def TxtImport(self):
+        self.fileName, self.filterName = QFileDialog.getOpenFileName(self)
+        self.textEdit.setText(open(self.fileName).read())
+
+    def logView(self):
+        self.log_window = LogWindow()
+        self.log_window.show()
+
+    def DatExport(self):
+        dat()
+
+    def TxtExport(self):
+        txt()
+
+    def DxfExport(self):
+        dxfe()
+
+    def CsvExport(self):
+        csv()
+
+    def XlsExport(self):
+        xls()
 
     def newFile(self):
-        self.textEdit.setText('')
+        print("Test New Projects")
 
     def exitFile(self):
         self.close()
 
-    def openFile(self): 
+    def openFile(self):
         self.fileName, self.filterName = QFileDialog.getOpenFileName(self)
         self.textEdit.setText(open(self.fileName).read())
 
     def saveFile(self):
-        if self.fileName == None or self.fileName == '':
-            self.fileName, self.filterName = QFileDialog.getSaveFileName(self, filter=self.filters)
-        if(self.fileName != ''):
-            file = open(self.fileName, 'w')
-            file.write(self.textEdit.toPlainText())
-            self.statusBar().showMessage("File saved", 2000)
+        self.fileName, self.filterName = QFileDialog.getSaveFileName(self)
 
     def CreateToolBar(self):
         self.mainToolBar = self.addToolBar('Main')
 
     def showAbout(self):
-        QMessageBox.about(self, "about",  "Foil Toolkit v0.1 is airfoil plotter written with Python")
+        QMessageBox.about(self, "about",  "Foil Toolkit v0.1.1 is open source airfoil plot generator\nIt's running on Windows and Linux environment \nDistribution and modification under GNU GPLv3 License\nReach author on contact@anggalab.com")
 
 class InputWindow(QDialog):
     def __init__(self):
         super(InputWindow, self).__init__()
         self.setFixedSize(220,180)
-        self.setWindowTitle('Input NACA')
+        self.setWindowTitle('NACA Input')
 
         layout = QVBoxLayout()
 
         self.labelnaca = QLabel("NACA Type")
         self.labelnaca.show()
         layout.addWidget(self.labelnaca)
-
-        self.nacainput = QLineEdit()
-        self.nacainput.setText('6409')
+        
+        self.nacainput = QLineEdit("6409")
         self.nacainput.show()
         layout.addWidget(self.nacainput)
 
         self.labelpoint = QLabel("Point Plot")
         self.labelpoint.show()
         layout.addWidget(self.labelpoint)
-
-        self.pointinput = QLineEdit()
-        self.pointinput.setText("400")
+        
+        self.pointinput = QLineEdit("500")
         self.pointinput.show()
         layout.addWidget(self.pointinput)
 
         self.plotBtn = QPushButton("Plot")
-        self.plotBtn.clicked.connect(self.input_naca_point)
         self.plotBtn.clicked.connect(self.input_naca_type)
+        self.plotBtn.clicked.connect(self.input_plot)
         self.plotBtn.show()
         layout.addWidget(self.plotBtn)
 
-        self.cancelBtn = QPushButton("Close")
+        self.cancelBtn = QPushButton("Cancel")
         self.cancelBtn.clicked.connect(self.input_close)
         self.cancelBtn.show()
         layout.addWidget(self.cancelBtn)
@@ -153,21 +198,61 @@ class InputWindow(QDialog):
         self.setLayout(layout)
         self.show()
 
+    def input_plot(self):
+        self.close()
+        MainWindow.close()
+        Popen('python launch.py')
+
     def input_close(self):
         self.close()
 
-    def input_naca_point(self):
-        input_point_text = self.pointinput.text()
-        input_point_text = int(input_point_text)
-        return input_point_text
-
     def input_naca_type(self):
         input_naca_text = self.nacainput.text()
-        return input_naca_text
+        input_point_text = self.pointinput.text()
+        with open('config.cfg', 'w') as txt_file:
+            txt_file.write("%s\n" % input_naca_text)
+            txt_file.write("%s" % input_point_text)
+        txt_file.close()
+
+class PropertiesWindow(QDialog):
+    def __init__(self):
+        super(PropertiesWindow, self).__init__()
+        self.setFixedSize(400,500)
+        self.setWindowTitle('Properties')
+
+        with open('config.cfg', 'r') as txt_file:
+            naca_properties = txt_file.read()
+
+        layout = QVBoxLayout()
+
+        self.naca_prop = QTextEdit(naca_properties)
+        self.naca_prop.show()
+        layout.addWidget(self.naca_prop)
+
+        self.setLayout(layout)
+        self.show()
+
+class LogWindow(QDialog):
+    def __init__(self):
+        super(LogWindow, self).__init__()
+        self.setFixedSize(500,450)
+        self.setWindowTitle('Log')
+
+        with open('data\data.log', 'r') as txt_file:
+            naca_properties = txt_file.read()
+
+        layout = QVBoxLayout()
+
+        self.naca_prop = QTextEdit(naca_properties)
+        self.naca_prop.show()
+        layout.addWidget(self.naca_prop)
+
+        self.setLayout(layout)
+        self.show()
 
 class PlotCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
+    def __init__(self, parent=None):
+        fig = Figure(figsize=(6.4, 5), dpi=100)
         self.axes = fig.add_subplot(111)
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
@@ -176,10 +261,15 @@ class PlotCanvas(FigureCanvas):
         self.airfoil()
 
     def airfoil(self, finite_TE = False, half_cosine_spacing = False):
-        trans_var = InputWindow()
-        nPoints = trans_var.input_naca_point()
-        profNaca = trans_var.input_naca_type()
-        profNaca = [profNaca]
+        file_input = open('config.cfg', mode='r')
+        type_line = file_input.readlines()
+        read_type = type_line[0]
+        read_node = type_line[0]
+        fileNaca = read_type.replace('\n', '')
+        fileNode = read_node.replace('\n', '')
+
+        nPoints = fileNode
+        profNaca = [fileNaca]
         naca_type = len(profNaca)
 
         ax = self.figure.add_subplot(111)
